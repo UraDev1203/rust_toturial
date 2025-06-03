babc71fd4d2a417a583069030f9a48f736f84027
sadneyBim <urarintaro0@gmail.com>
2025-06-02 19:52:01 -0700

    complete 8.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let mut v: Vec<i32> = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    let third: &i32 = &v[2];
+
+    println!("Hello, world! {third}");
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element.")
+    }
 }
