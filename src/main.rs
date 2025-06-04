5896958431c30c71eebf0820e692aa07d99d25c5
sadneyBim <urarintaro0@gmail.com>
2025-06-03 18:31:22 -0700

    complete str

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,16 @@ fn main() {
 
     let data = "initial contents";
     let mut s = data.to_string();
+    let s2 = "bar";
 
-    s.push_str("bar");
+    s.push_str(s2);
+    println!("s2 is {s2}");
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toc");
+
+    let s = format!("{s1}-{s2}-{s3}");
     println!("{s}");
 }
+
