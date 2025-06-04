a3eff32a70142648c00bddfbf0d36e646c40af74
sadneyBim <urarintaro0@gmail.com>
2025-06-03 17:55:49 -0700

    add vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String)
+}
 fn main() {
     let mut v: Vec<i32> = Vec::new();
     v.push(5);
@@ -14,4 +19,11 @@ fn main() {
         Some(third) => println!("The third element is {third}"),
         None => println!("There is no third element.")
     }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12)
+    ];
+    
 }
