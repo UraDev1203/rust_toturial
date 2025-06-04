cb9384e838efe3e4c84c6a16d7403d3059f183ed
sadneyBim <urarintaro0@gmail.com>
2025-06-03 18:11:05 -0700

    add string

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,9 @@
-enum SpreadsheetCell {
-    Int(i32),
-    Float(f64),
-    Text(String)
-}
+
 fn main() {
-    let mut v: Vec<i32> = Vec::new();
-    v.push(5);
-    v.push(6);
-    v.push(7);
-    v.push(8);
 
-    let third: &i32 = &v[2];
+    let data = "initial contents";
+    let mut s = data.to_string();
 
-    println!("Hello, world! {third}");
-
-    let third: Option<&i32> = v.get(2);
-    match third {
-        Some(third) => println!("The third element is {third}"),
-        None => println!("There is no third element.")
-    }
-
-    let row = vec![
-        SpreadsheetCell::Int(3),
-        SpreadsheetCell::Text(String::from("blue")),
-        SpreadsheetCell::Float(10.12)
-    ];
-    
+    s.push_str("bar");
+    println!("{s}");
 }
