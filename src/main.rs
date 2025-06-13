de29b20d1aeffb4507f29ab44f6824b2a3b2a175
uradev1203 <rintaroura3@gmail.com>
2025-06-12 18:07:26 -0700

    update error

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@
 
 fn main() {
     let string1 = String::from("abcd");
-    let string2 = String::from("xyz");  
+    let string2 = "xyz";  
     
-    let result = longest(x, y);
+    let result = longest(string1.as_str(), string2);
     println!("The longest string is {result}");
 }
 
-fn longest(x: &str, y: &str) -> &str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {x} else {y}
 }
