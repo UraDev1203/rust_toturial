3bb252657a68f898c00222c2c226d5a91d5af8dc
uradev1203 <rintaroura3@gmail.com>
2025-06-12 19:06:13 -0700

    complete chapter10

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+struct ImportantExcerpt<'a>{
+    part: &'a str,
+}
 
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {announcement}");
+        self.part
+    }
+}
 
 fn main() {
     let string1 = String::from("abcd");
@@ -6,6 +19,12 @@ fn main() {
     
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {result}");
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().unwrap();
+    let i = ImportantExcerpt {
+        part: first_sentence
+    };
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
