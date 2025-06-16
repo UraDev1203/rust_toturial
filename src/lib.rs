ed6e1e9168884a0f93445e3fbe2ef3e932d85061
uradev1203 <rintaroura3@gmail.com>
2025-06-15 17:45:44 -0700

    finish 11.1

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,33 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {value}.");
+        }
+
+        Guess { value }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn it_works() -> Result<(), String> {
         let result = add(2,2);
-        assert_eq!(result, 4);
+        if result == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+        // assert_eq!(result, 4);
     }
 
     #[test]
@@ -33,6 +52,12 @@ mod tests {
         let result = greeting("Carol");
         assert!(result.contains("Carol"));
 
+    }
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100(){
+        Guess::new(200);
     }
 }
 
